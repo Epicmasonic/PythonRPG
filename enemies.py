145d88e76b4a4467797471d577650dd2e2d60355
GroovyProto <90917323+Epicmasonic@users.noreply.github.com>
2026-03-11 21:36:00 -0400

    Refactor enemy turn logic and add Amalgam class

--- enemies.py
+++ enemies.py
@@ -33,7 +33,12 @@ class Inklin(Enemy):
 		super().__init__("Inklin" + suffix, 5, elemental_weaknesses=["Water"], elemental_resistances=["Poison", "Lie"])
 	
 	def take_turn(self, battle_handler):
-		party_member = random.choice(battle_handler.get_living_party_members())
+		living_party_members = []
+		for party_member in battle_handler.player_team:
+			if party_member.health > 0:
+				living_party_members.append(party_member)
+		
+		party_member = random.choice(living_party_members)
 		add_message(f"{self.name} bites at {party_member.name}.")
 		damage = party_member.take_damage(2)
 		
@@ -43,3 +48,38 @@ class Inklin(Enemy):
 			"Targets": [party_member],
 			"Damage Done": damage
 		}
+
+class Amalgam(Enemy):
+	def __init__(self,suffix=""):
+		super().__init__("Amalgam" + suffix, 30)
+	
+	def take_turn(self, battle_handler):
+		plan = {}
+		dice = random.randint(1, 2)
+		
+		if dice == 1:
+			plan["Team"] = "Player"
+			plan["Selected Target"] = random.choice(battle_handler.get_living_party_members())
+			plan["Targets"] = [random.choice(battle_handler.get_living_party_members())]
+		else:
+			plan["Team"] = "Enemy"
+			plan["Selected Target"] = random.choice(battle_handler.get_living_enemies())
+			plan["Targets"] = [random.choice(battle_handler.get_living_enemies())]
+		
+		dice = random.randint(1, 4)
+		if dice == 4:
+			if plan["Team"] == "Player":
+				plan["Targets"] = battle_handler.get_living_party_members()
+			else:
+				plan["Targets"] = battle_handler.get_living_enemies()
+		
+		
+		add_message(f"{self.name}... did... something?")
+		damage = random.randint(1, 10)
+		
+		plan["Damage Done"] = []
+		for battler in plan["Targets"]:
+			plan["Damage Done"].append(battler.take_turn(damage))
+		
+		plan["Caster"] = self
+		return plan
